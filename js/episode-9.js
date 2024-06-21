e4a4c2ea01ea75787dae36008d50e0f83bec1212
jeanclaudesperandezi <sounguejc@gmail.com>
2024-06-21 08:27:15 +0100

    modification de l'episode-9

--- js/episode-9.js
+++ js/episode-9.js
@@ -4,8 +4,8 @@ moyen = parseFloat(moyen);
 let message = (moyen >= 5 && moyen< 10) ? "mention passable" :
               (moyen >= 10 && moyen < 12) ? "mention assez-bien" :
               (moyen >= 13 && moyen < 14) ? "mention bien" :
-              (moyen >= 15 && moyen < 16) ? "mention tres-bien" :
-              (moyen >= 17 && moyen <= 20) ? "mention excellentte" :
+              (moyen >= 15 && moyen < 17) ? "mention tres-bien" :
+              (moyen >= 18 && moyen <= 20) ? "mention excellentte" :
               "mention null";
 
 alert(message);
